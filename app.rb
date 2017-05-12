059c697a3fd883269a111b1609b5861eb012831b
Ken Rutan <kenr16@gmail.com>
2017-05-12 10:06:29 -0700

    add full CRUD functionality to the brand object in the app.

--- app.rb
+++ app.rb
@@ -16,6 +16,8 @@ end
 
 get("/shoestores/:id") do
   @shoestore = Store.find(params['id'])
+  @list_brands = Brand.all
+  @store_brands = @shoestore.brands
   erb(:shoestore)
 end
 
@@ -35,11 +37,43 @@ end
 delete("/shoestores/:id") do
   shoestore = Store.find(params['id'])
   shoestore.destroy
-  redirect('/')
+  redirect('/shoestores')
 end
-
 
 get("/brands") do
   @brands = Brand.all
   erb(:brands)
+end
+
+get("/brands/:id") do
+  @brand = Brand.find(params['id'])
+  erb(:brand)
+end
+
+post("/brands") do
+  brand_name = params['brand_name']
+  brand_price = params['brand_price']
+  Brand.create({:name => brand_name, :price => brand_price})
+  redirect back
+end
+
+patch("/brands/:id") do
+  new_name = params['brand_name']
+  new_price = params['brand_price']
+  brand = Brand.find(params['id'])
+  brand.update({:name => new_name, :price => new_price})
+  redirect("/brands/#{brand.id}")
+end
+
+delete("/brands/:id") do
+  brand = Brand.find(params['id'])
+  brand.destroy
+  redirect('/brands')
+end
+
+post("/shoestores/:id") do
+  brand = Brand.find(params["brand_id"].to_i)
+  store = Store.find(params["id"].to_i)
+  Relationship.create({:brand_id => brand, :store_id => store.id})
+  redirect back
 end
