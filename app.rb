48226f424655a508c77fb58b5abe349dde41de0b
Ken Rutan <kenr16@gmail.com>
2017-05-12 15:20:24 -0700

    add ability to log in and log out to a user.  Parts of the site will now be off limits to users who have not yet logged in.

--- app.rb
+++ app.rb
@@ -99,26 +99,31 @@ post("/brands/:id/relationship") do
 end
 
 post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-        puts "Success!"
-        redirect back
-    else
-        puts "Failure."
-        erb(:security)
-    end
+  user = User.new(:username => params[:username], :password => params[:password])
+  if user.save
+    puts "Success!"
+    redirect back
+  else
+    puts "Failure."
+    erb(:security)
+  end
 end
 
 post "/login" do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        puts "Success!"
-        redirect back
-    else
-        puts "Failure."
-        erb(:security)
-    end
+  user = User.find_by(:username => params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    puts "Success!"
+    redirect back
+  else
+    puts "Failure."
+    erb(:security)
+  end
+end
+
+get "/logout" do
+  session[:user_id] = nil
+  redirect back
 end
 
 get "/security" do
